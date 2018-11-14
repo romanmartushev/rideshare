e96c36d45e4ac3e53122266646078c673c41a714
Roman <romanm@paulbunyan.net>
2018-11-14 11:38:20 -0600

    Revert "Implicitly setting scheme for route group"
    This reverts commit 67f8cc6a0d3cae126188b6d097a0fbf744a48b68.

--- routes/web.php
+++ routes/web.php
@@ -10,38 +10,36 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['scheme' => 'https'], function () {
-    Route::get('/', function () {
-        return redirect("/wordpress");
-    });
-
-    /**
-     * Routes for the admin/generic
-     */
-    Route::get('/apply', function () {
-        return view('partials.forms.apply');
-    });
-    Route::post('/submit-driver-form', 'ApplicationSubmitController@driverSubmit');
-    Route::post('/create/driver', 'ApplicationSubmitController@register');
-    Route::get('/fetch/drivers', function () {
-        if ($user = Auth::user()) {
-            return $user->getAllDrivers();
-        }
-        return [];
-    });
-    /**
-     * Routes for customers/drivers routes
-     */
-    Route::post('/customer/make-request', 'ApplicationSubmitController@customerSubmit');
-    Route::get('/fetch-request', function () {
-        if ($user = Auth::user()) {
-            return $user->getRequests();
-        }
-        return [];
-    });
-    /**
-     * Routes for RideShare
-     */
-    Auth::routes();
-    Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return redirect("/wordpress");
 });
+
+/**
+ * Routes for the admin/generic
+ */
+Route::get('/apply', function(){
+    return view('partials.forms.apply');
+});
+Route::post('/submit-driver-form', 'ApplicationSubmitController@driverSubmit');
+Route::post('/create/driver','ApplicationSubmitController@register');
+Route::get('/fetch/drivers',function (){
+    if($user = Auth::user()){
+        return $user->getAllDrivers();
+    }
+    return [];
+});
+/**
+ * Routes for customers/drivers routes
+ */
+Route::post('/customer/make-request', 'ApplicationSubmitController@customerSubmit');
+Route::get('/fetch-request', function () {
+    if($user = Auth::user()){
+        return $user->getRequests();
+    }
+    return [];
+});
+/**
+ * Routes for RideShare
+ */
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
